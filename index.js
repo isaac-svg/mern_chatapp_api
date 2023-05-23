b217a836df3f89241cec555cb98b437ab6f1a2c7
isaac-svg <isaacsakyi009@gmail.com>
2023-05-23 18:39:50 +0000

    commit

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
   credentials: true,
-  origin: "http://localhost:5173",
+  origin: process.env.ORIGIN,
 }));
 
 async function getUserDataFromRequest(req) {
